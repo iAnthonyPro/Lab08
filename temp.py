551e76da083f9e715448530a37e7eeb2f41dd611
Anthony <50153644+iAnthonyPro@users.noreply.github.com>
2022-03-27 19:15:31 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,9 @@
-userName = input ("Hello! Please enter your name!")
+userName = input ("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
 
-print ("Hello", userName, "nice to meet you")
+if(userName.lower() =="anthony" and userPassword == "Thinkr.Online"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
+    
